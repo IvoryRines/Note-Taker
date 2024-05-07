536b002fa83481fa50b1d7c1b3792fbb9300182d
EyeVorE <subplot_05tracer@icloud.com>
2024-05-06 22:43:23 -0500

    imported path. imported api routes from notes.js. Built get routes for notes.html and index.html.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { pageRouter } = require('./routes/pages');
+const path = require('path');
+const apiRoutes =  require('./routes/notes')
+
 
 const app = express();
 
@@ -7,7 +9,19 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', pageRouter);
+app.use(express.static('public'))
+
+app.use('/api', apiRoutes)
+
+// build a get route that goes to /notes and returns the notes.html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+// build a get route that goes to * and return the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
   console.info(`Server started on http://localhost:${PORT}`);
