8f8ace0e50ddf69d79490a674a47e7712b04d2f4
EyeVorE <subplot_05tracer@icloud.com>
2024-05-07 02:01:45 -0500

    cleaned up spacing and added missing semi colons.

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,4 @@
-const router =  require('express').Router()
+const router =  require('express').Router();
 const fs = require('fs');
 
 const uuid = require('../db/uuid');
@@ -37,5 +37,6 @@ router.post('/notes', (req, res) => {
 
   res.json(data); // Respond with the updated data
 });
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,26 +2,27 @@ const express = require('express');
 const path = require('path');
 const apiRoutes =  require('./routes/notes')
 
-
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use('/api', apiRoutes)
+app.use('/api', apiRoutes);
 
 // build a get route that goes to /notes and returns the notes.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+});
+
 
 // build a get route that goes to * and return the index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
-})
+});
+
 
 app.listen(PORT, () => {
   console.info(`Server started on http://localhost:${PORT}`);
